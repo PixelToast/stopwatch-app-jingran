ab5e500dd34fcde244a944d8f30d9972468c4927
Jingran Wang <hikaritenchi@gmail.com>
2021-07-21 16:39:41 -0400

    Add tests covering all current functionality
    Lap button starts out disabled
    Timer text starts out initialized
    Start button enables lap button
    Timer text is increased by the correct amount
    Lap button functionality generates correct row text
    Stop button disables lap button and reinitializes timer text
    Start button clears out lap rows

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,14 +7,94 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:stopwatch_app_jingran/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('app flow', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
+    String getTimerText() {
+      return (tester.widget<Text>(find.byKey(Key('timer.display')))).data!;
+    }
+
+    expect(
+      tester.widget<ElevatedButton>(find.byKey(Key('lap.button'))).enabled,
+      isFalse,
+    );
+
+    expect(
+      getTimerText(),
+      "00:00.0",
+    );
+
+    var startButton = find.byKey(Key("start.button"));
+    await tester.tap(startButton);
+    await tester.pump();
+
+    var lapButton = find.byKey(Key("lap.button"));
+    expect(
+      tester.widget<ElevatedButton>(lapButton).enabled,
+      isTrue,
+    );
+
+    await tester.pump(const Duration(milliseconds: 8100));
+
+    expect(
+      getTimerText(),
+      "00:08.1",
+    );
+
+    await tester.tap(lapButton);
+    await tester.pump();
+
+    expect(
+      find.text("Lap 1: 00:08.1"),
+      findsOneWidget,
+    );
+
+    await tester.pump(const Duration(milliseconds: 3300));
+
+    expect(
+      getTimerText(),
+      "00:11.4",
+    );
+
+    await tester.tap(lapButton);
+    await tester.pump();
+
+    expect(
+      find.text("Lap 1: 00:08.1"),
+      findsOneWidget,
+    );
+
+    expect(
+      find.text("Lap 2: 00:11.4"),
+      findsOneWidget,
+    );
+
+    await tester.tap(find.byKey(Key("stop.button")));
+    await tester.pump();
+
+    expect(
+      tester.widget<ElevatedButton>(find.byKey(Key('lap.button'))).enabled,
+      isFalse,
+    );
+
+    expect(
+      getTimerText(),
+      "00:00.0",
+    );
+
+    await tester.tap(startButton);
+    await tester.pump();
+
+    expect(
+      find.text("Lap 2: 00:11.4"),
+      findsNothing,
+    );
+
+  });
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
